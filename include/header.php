89e12e4f7136994cf3cda3b4301dafcbf2e5d8ae
nath54 <cerisara.nathan@gmail.com>
2021-12-24 12:22:32 +0100

    css qui marche ENFIN !

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 <header class="row">
-    <div style="margin-top:auto; margin-bottom:auto; margin-left:10px;">
-        <a class="menu_3bandes" href="#">&#9776;</a>
+    <div id="bt_hamb" style="margin-top:auto; margin-bottom:auto; margin-left:10px;">
+        <button class="menu_3bandes" href="#" onclick="if(this.classList.contains('menu_3bandes_down') ){ this.classList.remove('menu_3bandes_down'); }else{ this.classList.add('menu_3bandes_down'); }">&#9776;</button>
     </div>
     <div style="width:auto; margin: auto;">
         <h2>Collab Work</h2>
