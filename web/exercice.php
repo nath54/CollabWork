df33699e7adb06e91960e1440bc20fdeb23237e3
nath54 <cerisara.nathan@gmail.com>
2021-12-27 15:08:43 +0100

    aa

--- web/exercice.php
+++ web/exercice.php
@@ -96,10 +96,10 @@ $reponses = [
                                 echo "<div class=\"row\">";
                                 echo "  <p id=\"reponse_q_$id\">$txt_reponse</p>";
                                 echo "  <textarea id=\"input_reponse_q_$id\" style=\"display:none; margin-left:2vh; margin-top:auto; margin-bottom:auto;\">$txt_reponse</textarea>";
-                                echo "  <div id=\"bt_repondre_q_$id\" style='margin-left:2vh; margin-top:auto; margin-bottom:auto; $aff_bt_repondre'> <button  class=\"bt1\">Répondre</button> </div>";
-                                echo "  <div id=\"bt_modifier_q_$id\" style='margin-left:2vh; margin-top:auto; margin-bottom:auto; $aff_bt_modif'> <button class=\"bt1\">Modifier</button> </div>";
-                                echo "  <div id=\"bt_sauvegarder_q_$id\" style='display:none; margin-left:2vh; margin-top:auto; margin-bottom:auto; '> <button class=\"bt1\">Sauvegarder</button> </div>";
-                                echo "  <div id=\"bt_annuler_q_$id\" style='display:none; margin-left:2vh; margin-top:auto; margin-bottom:auto; '> <button class=\"bt1\">Annuler</button> </div>";
+                                echo "  <div id=\"bt_repondre_q_$id\" style='margin-left:2vh; margin-top:auto; margin-bottom:auto; $aff_bt_repondre'> <button onclick='modif_reponse($id);' class=\"bt1\">Répondre</button> </div>";
+                                echo "  <div id=\"bt_modif_q_$id\" style='margin-left:2vh; margin-top:auto; margin-bottom:auto; $aff_bt_modif'> <button onclick='modif_reponse($id);' class=\"bt1\">Modifier</button> </div>";
+                                echo "  <div id=\"bt_save_q_$id\" style='display:none; margin-left:2vh; margin-top:auto; margin-bottom:auto; '> <button class=\"bt1\">Sauvegarder</button> </div>";
+                                echo "  <div id=\"bt_annule_q_$id\" style='display:none; margin-left:2vh; margin-top:auto; margin-bottom:auto; '> <button onclick='annule_reponse($id);' class=\"bt1\">Annuler</button> </div>";
                                 echo "</div>";
                             }
                             echo "</div>";
@@ -108,8 +108,21 @@ $reponses = [
 
                     ?>
 
-                </div>
+                    <div style="margin-top:10vh;">
+                        <?php
+                            
+                            if(false){
+                                echo `<button id="bt_aff_reponses" class="bt1" style="display:block; margin-bottom:2vh;" onclick="toggle_reponses();">Afficher les réponses</button>`;
+                            }
+                            else{
+                                echo "<i>Cet exercice n'a pas de corrigé</i>";
+                            }
+                        
+                        ?>
+                    </div>
 
+                </div>
+                
 
             </div>
 
@@ -123,8 +136,44 @@ $reponses = [
     <script src="../js/tex-mml-chtml.js"></script>
     <script>
 
-function repondre_question(idq){
-    
+function modif_reponse(idq){
+    var p = document.getElementById("reponse_q_"+idq);
+    var input = document.getElementById("input_reponse_q_"+idq);
+    // input.value = p.innerHTML;
+    var bt_repondre = document.getElementById("bt_repondre_q_"+idq);
+    var bt_modif = document.getElementById("bt_modif_q_"+idq);
+    var bt_save = document.getElementById("bt_save_q_"+idq);
+    var bt_annule = document.getElementById("bt_annule_q_"+idq);
+    //
+    p.style.display = "none";
+    input.style.display = "block";
+    bt_repondre.style.display = "none";
+    bt_modif.style.display = "none";
+    bt_save.style.display = "block";
+    bt_annule.style.display = "block";
+}
+
+function annule_reponse(idq){
+    var p = document.getElementById("reponse_q_"+idq);
+    var input = document.getElementById("input_reponse_q_"+idq);
+    // p.innerHTML = input.value;
+    var bt_repondre = document.getElementById("bt_repondre_q_"+idq);
+    var bt_modif = document.getElementById("bt_modif_q_"+idq);
+    var bt_save = document.getElementById("bt_save_q_"+idq);
+    var bt_annule = document.getElementById("bt_annule_q_"+idq);
+    //
+    p.style.display = "block";
+    input.style.display = "none";
+    if(input.value.length == 0){
+        bt_repondre.style.display = "block";
+        bt_modif.style.display = "none";
+    }
+    else{
+        bt_repondre.style.display = "none";
+        bt_modif.style.display = "block";
+    }
+    bt_save.style.display = "none";
+    bt_annule.style.display = "none";
 }
 
 
