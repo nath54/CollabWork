b09195aa7c1ee25248b5b82f85c639c7823757aa
nath54 <cerisara.nathan@gmail.com>
2022-01-10 18:23:04 +0100

    aa

---
+++ web/disconnect.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once "../include/init.php";
+include_once "../include/bdd.php";
+
+$db = load_db();
+
+include "../include/test_connecte.php";
+
+
+
+if($est_connecte){
+    $req = "UPDATE comptes SET token='' WHERE id=:id_compte";
+    action_prep($db, $req, [":id_compte"=>$_SESSION["id_compte"]])
+}
+
+
+unset($_SESSION["est_connecte"]    );
+unset($_SESSION["pseudo"]          );
+unset($_SESSION["token_connection"]);
+unset($_SESSION["id_compte"]       );
+
+
+
+?>
