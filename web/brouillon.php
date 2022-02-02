2d0d251588124c7d0a49f610688b7e7ea1929273
nathan cerisara <cerisara.nathan@gmail.com>
2022-02-02 16:54:59 +0100

    aa

--- web/brouillon.php
+++ web/brouillon.php
@@ -95,7 +95,7 @@ $_SESSION["last_page"] = "brouillon.php";
                         <button id="bt_view" onclick="view();" class="bt_tab">Voir</button>
                     </div>
                     <div class="row" style="height:85%; margin-bottom:2vh;">
-                        <div id="div_textarea" style="width:100%; height:100%;">
+                        <div id="div_textarea" style="max-width:100%; height:100%;">
                             <textarea id="texte" style="width:100%; height:100%; border: 1px solid grey;" placeholder="Ecrire son texte ici" onkeyup="key_compile();"><?php echo  urldecode($texte); ?></textarea>
                         </div>
                         <div id="div_result">
