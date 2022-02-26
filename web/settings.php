46674c289c457a7157b69f72c684c58993fc3c19
nathan cerisara <cerisara.nathan@gmail.com>
2022-02-26 11:32:59 +0100

    aa

--- web/settings.php
+++ web/settings.php
@@ -36,6 +36,10 @@ $_SESSION["last_page"] = "main.php";
 
         <div id="main_body" style="position: absolute; top:8%; bottom: 0; left: 0; right: 0; overflow:auto;">
 
+            <div>
+                <h1>Paramètres : </h1>
+            </div>
+
             <!-- Mode non auteur -->
             <div>
                 <button id="mode_non_auteur" class="btn btn-primary" onclick="send_form('settings.php', [['type', 'toggle_mode_non_auteur']]);"><?php if(isset($_SESSION["mode_non_auteur"])){ echo "Désactiver"; } else { echo "Activer"; } ?> le mode non auteur (session)</button>
