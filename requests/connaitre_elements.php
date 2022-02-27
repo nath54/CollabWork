33231921bd8c4c4abafd3f11665eeebfdb330a22
nathan cerisara <cerisara.nathan@gmail.com>
2022-02-27 12:46:00 +0100

    aa

--- requests/connaitre_elements.php
+++ requests/connaitre_elements.php
@@ -1,8 +1,13 @@
 <?php
 
-if(isset($_POST["elts"]) && test_token($_POST)){
+if(isset($_POST["elts"]) && $est_connecte && test_token($_POST)){
     alert("Ca marche !");
-    
+    $elts = json_decode($_POST["elts"], true);
+    foreach($elts as $elt_id){
+        // On va désactiver cet élément
+        $req = "INSERT INTO elements_connus (id_compte, id_element) VALUES (:id_compte, :id_element);";
+        action_prep($db, $req, [":id_compte"=>$_SESSION["id"], ":id_element"=>$elt_id]);
+    }
 }
 
 ?>
